1b2100db09695e44090020dff27b38cd4daa60d2
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-05 17:53:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,11 @@ def countdown(seconds)
     while seconds > 0
       puts "#{seconds} SECOND(S)!"
       seconds -= 1
+      countdown_with_sleep
     end
     return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(1)
+end
+     
