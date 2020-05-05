ea6ee1685a0cbef62b2b2f7c0c74510c67a580dc
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-05 17:58:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,10 @@
 #write your code here
 def countdown(seconds)
-    seconds = 10
+    seconds = 5
     while seconds > 0
       puts "#{seconds} SECOND(S)!"
       seconds -= 1
-      countdown_with_sleep
+      sleep(1)
     end
     return "HAPPY NEW YEAR!"
 end
-
-def countdown_with_sleep(1)
-end
-     
